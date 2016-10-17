f6889cf559395f2f294162c2f469d06931da4050
Ben Venezia <benven@post.bgu.ac.il>
2016-10-17 13:00:30 +0300

    Added MyPosts menu option
    Added My Posts menu icon

--- app/src/main/java/net/cloudapp/chooser/chooser/views/MyPosts/MyPostsView.java
+++ app/src/main/java/net/cloudapp/chooser/chooser/views/MyPosts/MyPostsView.java
@@ -1,12 +1,9 @@
-package net.cloudapp.chooser.chooser.views;
+package net.cloudapp.chooser.chooser.views.MyPosts;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by t-baya on 10/10/2016.
- */
 
 public class MyPostsView extends AppCompatActivity {
     @Override
