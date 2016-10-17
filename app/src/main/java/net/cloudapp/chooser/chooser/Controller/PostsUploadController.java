cfe843baba0cdcaa7456d0692e24ee06c524e12d
Barak Yaari <barakyaari@gmail.com>
2016-10-17 14:07:15 +0300

    Fixed image not uploading bug

--- app/src/main/java/net/cloudapp/chooser/chooser/Controller/PostsUploadController.java
+++ app/src/main/java/net/cloudapp/chooser/chooser/Controller/PostsUploadController.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 
 import net.cloudapp.chooser.chooser.Common.SessionDetails;
+import net.cloudapp.chooser.chooser.Images.CloudinaryClient;
 import net.cloudapp.chooser.chooser.Images.ImageUploader;
 import net.cloudapp.chooser.chooser.Images.ImageUploaderImpl;
 import net.cloudapp.chooser.chooser.Network.RestFramework.RestClient;
@@ -12,8 +13,10 @@ import net.cloudapp.chooser.chooser.Controller.Callbacks.PostUploadCallback;
 public class PostsUploadController {
 
     public void uploadPost(Context context, String title, Bitmap image1BitMap, Bitmap image2BitMap, String description1, String description2){
-        ImageUploader uploader = new ImageUploaderImpl();
         String image1Id = "", image2Id = "";
+        ImageUploader uploader = new ImageUploaderImpl();
+        image1Id = uploader.uploadImage(image1BitMap);
+        image2Id = uploader.uploadImage(image2BitMap);
         RestClient restClient = new RestClient();
         restClient.getService().addpost(
                 SessionDetails.getInstance().getAccessToken().getToken(),
