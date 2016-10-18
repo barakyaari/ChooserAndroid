61fcade49274ad5d8c0509fedf5654b53cd6898b
Ben Venezia <benven@post.bgu.ac.il>
2016-10-19 02:29:00 +0300

    Rearranged entire statistics code to fit project

---
+++ app/src/main/java/net/cloudapp/chooser/chooser/Common/StatisticsChartSetup.java
@@ -0,0 +1,7 @@
+package net.cloudapp.chooser.chooser.Common;
+
+/**
+ * Created by Ben on 19/10/2016.
+ */
+public class StatisticsChartSetup {
+}
---
+++ app/src/main/java/net/cloudapp/chooser/chooser/views/dialogs/AgeFragmentIntervalDialog.java
@@ -0,0 +1,7 @@
+package net.cloudapp.chooser.chooser.views.dialogs;
+
+/**
+ * Created by Ben on 19/10/2016.
+ */
+public class AgeFragmentIntervalDialog {
+}
