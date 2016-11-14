08f51b47ca9f075841b788a89d3c7e88497f725f
Ben Venezia <benven@post.bgu.ac.il>
2016-11-14 18:33:31 +0200

    Implemented notification Services

---
+++ app/src/main/java/net/cloudapp/chooser/chooser/NotificationServices/ChooserFirebaseIdService.java
@@ -0,0 +1,8 @@
+package net.cloudapp.chooser.chooser.NotificationServices;
+
+/**
+ * Created by Ben on 14/11/2016.
+ */
+
+public class ChooserFirebaseIdService {
+}
---
+++ app/src/main/java/net/cloudapp/chooser/chooser/notificationServices/Notifier.java
@@ -0,0 +1,8 @@
+package net.cloudapp.chooser.chooser.notificationServices;
+
+/**
+ * Created by Ben on 14/11/2016.
+ */
+
+public class Notifier {
+}
